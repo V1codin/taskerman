e4494c0611f3650e3bb3fb23d45507ae521b6a55
kir <blindmonk46@gmail.com>
2023-04-05 00:39:38 +0300

    feat: deleting board modal window

--- src/views/Header/logged/components/modals/DeleteBoardModal.tsx
+++ src/views/Header/logged/components/modals/DeleteBoardModal.tsx
@@ -1,16 +1,55 @@
 import Confirm from '@/modules/confirm/Confirm';
 
-type DeleteBoardModalProps = {
-  boardTitle?: string;
-};
+import { getSetBoardsState, getSetModal } from '@/context/stateManager';
+import { TDeleteModalData } from '@/types/state';
+import { deleteBoard } from '@/utils/api/boards';
+import { useSetAtom } from 'jotai';
+import { SyntheticEvent, useCallback } from 'react';
 
-const DeleteBoardModal: React.FC<DeleteBoardModalProps> = ({ boardTitle }) => {
+type DeleteBoardModalProps = TDeleteModalData;
+
+const DeleteBoardModal: React.FC<DeleteBoardModalProps> = ({ id, text }) => {
+  const setModal = useSetAtom(getSetModal);
+  const setBoards = useSetAtom(getSetBoardsState);
+
+  const refreshData = useCallback(
+    (boardId: string) => {
+      setBoards((state) => state.filter((item) => item._id !== boardId));
+    },
+    [setBoards],
+  );
+
+  const accept = useCallback(
+    async (e: SyntheticEvent<HTMLElement>) => {
+      e.preventDefault();
+      try {
+        await deleteBoard(id);
+        refreshData(id);
+
+        setModal({
+          isOpen: false,
+          window: null,
+        });
+      } catch (e) {
+        console.error('error', e);
+      }
+    },
+    [id, refreshData, setModal],
+  );
+
+  const decline = useCallback(() => {
+    setModal({
+      isOpen: false,
+      window: null,
+    });
+  }, [setModal]);
+
   return (
-    <Confirm>
-      {boardTitle ? (
+    <Confirm accept={accept} decline={decline}>
+      {text ? (
         <h3>
           Delete the board, titled -{' '}
-          <span style={{ color: 'red' }}>{boardTitle}</span>?
+          <span style={{ color: 'red' }}>{text}</span> ?
         </h3>
       ) : (
         <h3>Confirm the board deleting</h3>
