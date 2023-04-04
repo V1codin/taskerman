ed1cb399f116a8f4787d3ba530dd2875a499ebce
kir <blindmonk46@gmail.com>
2023-04-05 00:36:20 +0300

    add: server handler for client's session

---
+++ src/pages/api/auth/session.ts
@@ -0,0 +1,56 @@
+import { getServerSession } from 'next-auth/next';
+import { authOptions } from './[...nextauth]';
+import { ServerResponseError } from '@/libs/error.service';
+import { TRawUserBoards, boardService } from '@/libs/boards.service';
+
+import type { TError } from '@/types/state';
+import type { NextApiRequest, NextApiResponse } from 'next/types';
+import type { SessionUser } from '@/types/db';
+
+import { dbConnect } from '@/libs/db/connect';
+
+export type TAuthenticatedUser = {
+  user: SessionUser;
+  boards: TRawUserBoards;
+};
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<TAuthenticatedUser | TError>,
+) {
+  try {
+    const session = await getServerSession(req, res, authOptions);
+    if (session) {
+      await dbConnect();
+      const boards = await boardService.getUserBoards(session.user.id);
+
+      res.status(200).json({
+        user: session.user,
+        boards,
+      });
+
+      return;
+    }
+    throw new ServerResponseError({
+      code: 403,
+      message: 'Error: Unauthorized',
+    });
+  } catch (e) {
+    if (e instanceof ServerResponseError) {
+      res.status(e.code).json({
+        message: e.message,
+        code: e.code,
+      });
+
+      return;
+    }
+    res.status(500).json({
+      code: 500,
+      message: 'Error: Server does not response',
+    });
+  }
+}
+
+export const config = {
+  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
+};
