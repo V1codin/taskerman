b543c673e8cf6994771823805d9f3f52f7d85514
kir <blindmonk46@gmail.com>
2023-04-05 16:23:44 +0300

    feat: data base provider interface  for possibility of migration to another db

--- src/libs/db/mongo.ts
+++ src/libs/db/mongo.ts
@@ -2,10 +2,71 @@ import Users from '@/models/users';
 import Boards from '@/models/boards';
 
 import { Types } from 'mongoose';
-import { TCreatingBoard } from '@/types/db';
+import type { IBoard } from '@/models/boards';
+import type { FilterQuery } from 'mongoose';
+import type { TCreatingBoard } from '@/types/db';
 
-export class MongoDataBaseProvider {
+interface DataBaseProvider<
+  ParticularDBType extends unknown,
+  TBoardQuery,
+  TUserByName = unknown,
+  TUserById = unknown,
+  TUserIdByUserName = unknown,
+  TBoarById = unknown,
+  TUserBoards = unknown,
+  TCreatedBoard = unknown,
+  TDeletedBoard = unknown,
+> {
+  getBoardsQueryUtils(userId: string): TBoardQuery;
+
+  getUserByUserName(username: string): TUserByName;
+  getUserById(userId: string | ParticularDBType): TUserById;
+  getUserIdByUserName(username: string): TUserIdByUserName;
+  getBoardById(boerdId: string | ParticularDBType): TBoarById;
+  getUserBoards(
+    query: TBoardQuery | null,
+    userId?: string | ParticularDBType,
+  ): TUserBoards;
+  createBoard(board: TCreatingBoard): TCreatedBoard;
+  deleteBoard(boardId: string | ParticularDBType): TDeletedBoard;
+}
+
+export class MongoDataBaseProvider
+  implements
+    DataBaseProvider<
+      Types.ObjectId,
+      FilterQuery<IBoard>,
+      ReturnType<typeof Users.findOne>,
+      ReturnType<typeof Users.findOne>,
+      ReturnType<typeof Users.findOne>,
+      ReturnType<typeof Boards.findOne>,
+      ReturnType<typeof Boards.find>,
+      // ? unknown because Boards.create is overloading function
+      // ? and ReturnType is not compatible
+      unknown,
+      ReturnType<typeof Boards.deleteOne>
+    >
+{
   constructor() {}
+
+  /*
+  ? Util method for changing DataProvider but save request data API
+  ? example: 
+  *  class YourDataBaseProvider {
+  *  getBoardsQueryUtils(): YourDataBaseQuery
+  *  getUserBoards(query): Boards -> makes request to db relies YourDataBaseQuery
+  * }
+  */
+  getBoardsQueryUtils(userId: string | Types.ObjectId): FilterQuery<IBoard> {
+    return {
+      $or: [
+        {
+          ownerId: userId,
+        },
+        { memberIds: userId },
+      ],
+    };
+  }
 
   getUserByUserName(username: string) {
     const user = Users.findOne({
@@ -39,13 +100,18 @@ export class MongoDataBaseProvider {
     return result;
   }
 
-  getUserBoards(userId: string | Types.ObjectId) {
+  getUserBoards(
+    query: FilterQuery<IBoard> | null,
+    userId?: string | Types.ObjectId,
+  ) {
+    if (query) {
+      return Boards.find(query);
+    }
+
     const _id =
       typeof userId === 'string' ? new Types.ObjectId(userId) : userId;
 
-    const boards = Boards.find({
-      ownerId: _id,
-    });
+    const boards = Boards.find({ ownerId: _id });
 
     return boards;
   }
