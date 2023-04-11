99368b59e8682c5fd88434fe827a5550aa73ef58
kir <blindmonk46@gmail.com>
2023-04-11 17:07:12 +0300

    add: avatar component

---
+++ src/modules/avatar/Avatar.tsx
@@ -0,0 +1,33 @@
+import DefaultAccountImage from '@/views/Header/logged/components/DefaultAccountImage';
+import ImageModule from '../image/Image';
+
+type AvatarProps = {
+  imageURL?: string;
+  displayName?: string;
+  username?: string;
+  avatarWidth?: number;
+  avatarHeight?: number;
+  className?: string;
+};
+
+const Avatar: React.FC<AvatarProps> = ({
+  imageURL,
+  displayName,
+  username,
+  className,
+  avatarWidth = 38,
+  avatarHeight = 38,
+}) => {
+  return imageURL ? (
+    <ImageModule
+      src={imageURL}
+      width={avatarWidth}
+      height={avatarHeight}
+      className={className}
+      alt="User avatar"
+    />
+  ) : (
+    <DefaultAccountImage userAliases={{ displayName, username }} />
+  );
+};
+export default Avatar;
