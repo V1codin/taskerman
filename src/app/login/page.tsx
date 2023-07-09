cfa74a359a68b1da81964c37614fd3aae33f67e1
kir <blindmonk46@gmail.com>
2023-07-09 21:11:25 +0300

    \n

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,7 +2,7 @@ import LoginForm from '@/modules/modal/modalForms/authForms/LoginForm';
 
 import { authService } from '@/libs/auth.service';
 import { dbConnect } from '@/libs/db/connect';
-import { AUTH_TOKEN_COOKIE_NAME, CURRENT_DB } from '@/utils/constants';
+import { AUTH_TOKEN_COOKIE_NAME } from '@/utils/constants';
 import { cookies } from 'next/headers';
 import { redirect } from 'next/navigation';
 
